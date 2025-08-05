ebdd163db19018be7949b2dfa4f0f284bf232185
novasistemas <davije.nova@gmail.com>
2025-08-04 21:36:31 -0600

    Add files via upload

--- Js/footer.js
+++ Js/footer.js
@@ -4,9 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
       <div class="container">
             <div class="row g-4">
                 <div class="col-lg-4">
-                    <h5><i class="fas fa-chart-line me-2"></i>FinTech Solutions</h5>
-                    <p class="mb-3">Soluciones tecnológicas especializadas para el sector financiero mexicano. 
-                    Transformamos la manera en que las instituciones financieras operan y cumplen con la regulación.</p>
+                    <h5><i class="fas fa-chart-line me-2"></i>FinTech Services</h5>
+                    <p class="mb-3">Servicios de Tecnología Especializada en el Sector Financiero. 
+                    Automatizando los procesos y el cumplimiento de la normativa.</p>
                     <div class="social-links">
                         <a href="#" class="me-3"><i class="fab fa-linkedin fa-lg"></i></a>
                         <a href="#" class="me-3"><i class="fab fa-twitter fa-lg"></i></a>
@@ -16,41 +16,34 @@ document.addEventListener("DOMContentLoaded", function() {
                 <div class="col-lg-2">
                     <h5>Servicios</h5>
                     <ul class="list-unstyled">
-                        <li><a href="#servicios">Cartera de Créditos</a></li>
-                        <li><a href="#servicios">Contratos</a></li>
-                        <li><a href="#servicios">Contabilidad</a></li>
-                        <li><a href="#servicios">PLD/EBR</a></li>
-                        <li><a href="#servicios">Reportes</a></li>
+                        <li><a href="servicios.html">Cartera de Créditos</a></li>
+                        <li><a href="servicios.html">Contratos</a></li>
+                        <li><a href="servicios.html">Contabilidad</a></li>
+                        <li><a href="servicios.html#pld">PLD/EBR</a></li>
+                        <li><a href="servicios.html#pld">Reportes</a></li>
                     </ul>
                 </div>
                 <div class="col-lg-2">
                     <h5>Empresa</h5>
                     <ul class="list-unstyled">
-                        <li><a href="#nosotros">Nosotros</a></li>
-                        <li><a href="#nosotros">Equipo</a></li>
-                        <li><a href="#contacto">Contacto</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">Casos de Éxito</a></li>
+                        <li><a href="nosotros.html">Nosotros</a></li>
+                        <li><a href="nosotros.html#team">Equipo</a></li>
+                        <li><a href="contacto.html">Contacto</a></li>
+                        <li><a href="servicios.html#testimonial">Casos de Éxito</a></li>
                     </ul>
                 </div>
                 <div class="col-lg-2">
                     <h5>Soporte</h5>
                     <ul class="list-unstyled">
-                        <li><a href="#">Centro de Ayuda</a></li>
-                        <li><a href="#">Documentación</a></li>
-                        <li><a href="#">API</a></li>
-                        <li><a href="#">Estado del Servicio</a></li>
-                        <li><a href="#">Actualizaciones</a></li>
+                        <li>Lunes - Viernes: </li>
+                        <li>de 9:00 a 17:00</li>
                     </ul>
                 </div>
                 <div class="col-lg-2">
                     <h5>Legal</h5>
                     <ul class="list-unstyled">
-                        <li><a href="#">Términos de Uso</a></li>
-                        <li><a href="#">Privacidad</a></li>
-                        <li><a href="#">Cookies</a></li>
+                        <li><a href="privacidad.html">Privacidad</a></li>
                         <li><a href="#">Licencias</a></li>
-                        <li><a href="#">Compliance</a></li>
                     </ul>
                 </div>
             </div>
--- Js/navbar.js
+++ Js/navbar.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const navbarHTML = `
     <nav class="navbar navbar-expand-lg fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="index.html">FinTech Solutions</a>
+        <a class="navbar-brand" href="index.html">FinTech Services</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span class="navbar-toggler-icon"></span>
         </button>
